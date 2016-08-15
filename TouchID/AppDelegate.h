c0fa92d8a239654c10ad2534dfcc891244e4c08b
手机-HGL <pingtong@shujincai.local>
2016-08-15 14:07:03 +0800

    Initial Commit

---
+++ TouchID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchID
+//
+//  Created by pingtong on 16/8/15.
+//  Copyright © 2016年 PTDriver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
